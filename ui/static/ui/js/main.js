e3fec29f9bbae82ba16730c37f8b5c6f66ea3214
VadimWebworm <Vadim-030101@yandex.ru>
2023-01-07 18:03:49 +0300

    add UI/UX

--- ui/static/ui/js/main.js
+++ ui/static/ui/js/main.js
@@ -1,21 +1,56 @@
 $(function () {
-    $(".screen-height").height($(window).height());
+    var heightHeaderFooter = $(".header").height();
+    $(".screen-height").height($(window).height() - heightHeaderFooter);
 
-    $(window).resize(function () {
-        $(".screen-height").height($(window).height());
-    });
-
+    // $(window).resize(function () {
+    //     $(".screen-height").height($(window).height());
+    // });
+    if(!$(".btnPrev").length){
+        $('.quizInnerBtn').addClass('quizInnerNonBtn');
+        }
     $(".start-stop").click(function () {
-        $('.jumbotron__inner-item p').removeClass('animation-true').eq($(this).index()).addClass("animation-true");
-        $("button").removeClass("active").eq($(this).index()).addClass("active");
+        $('.quizInnerItem h4').removeClass('animation-true').eq($(this).index() - 1).addClass("animation-true");
+        $("button").removeClass("active").eq($(this).index() - 1).addClass("active");
+        if(!$(".btnPrev").length){
+            $('.quizInnerBtn').addClass('quizInnerNonBtn');
+            }
+        if ($("#stop").hasClass("active")){
+            $(".btnNext").removeClass("nonvision");
+            if(!$(".btnPrev").length){
+                $('.quizInnerBtn').removeClass('quizInnerNonBtn');
+                }
+        }else{
+            $(".btnNext").addClass("nonvision");
+        }
     });
-    $(".pause-button").click(function () {
-        $('.jumbotron__inner-item p').toggleClass('animation-true');
-        $(".pause-button").toggleClass('active');
+    // $("#stop").click(function(){
         
-    });
-    $("#stop").click(function () {
-        $('#quest').load(document.URL + ' #quest');
+    // })
+
+    $('.sliderInner').slick({
+        fade: true,
+        arrows: false,
+        focusOnSelect: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 2500,
     });
 });
+
+// let btn = document.querySelector('.btnNext');
+// let stop = document.getElementById('stop');
+// stop.addEventListener("click", (e) => {
+//     stop.classList.add('time')
+//     console.log(document.getElementById('stop').classList)
+//     console.log('yes')
+//     btn.classList.remove('nonvision')
+    
+
+
+// })
+// console.log(stop.classList)
+
+
+
+
 
