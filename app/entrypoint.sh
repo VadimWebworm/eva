4f501d11976cf13baf63c288deda210b1fcb45eb
labintsev <andrej.labintsev@yandex.ru>
2023-03-15 13:20:42 +0300

    change host name to eva.mldl.ru

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -16,5 +16,6 @@ python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata fixtures/data.json fixtures/users.json
+python manage.py collectstatic --no-input --clear
 
 exec "$@"
--- app/eva/settings.py
+++ app/eva/settings.py
@@ -18,6 +18,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -129,12 +135,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
